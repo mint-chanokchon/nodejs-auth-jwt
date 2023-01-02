74915954d874fb56299883fed8ee787bd1c3b9cf
Chanokchon Wongjumpa <121343656+mint-chanokchon@users.noreply.github.com>
2023-01-02 21:22:59 +0700

    add check when some value is undefined

--- server.js
+++ server.js
@@ -16,10 +16,22 @@ const connection = mysql.createConnection({
     }
     console.log('Connection successful')
 });
-
+const response = { message: null, data: null }
 
 const app = express()
 
 app.use(cors())
+
+app.post('/register', (req, res) => {
+    const email = req.body?.email
+    const password = req.body?.password
+    const fname = req.body?.fname 
+    const lname = req.body?.lname 
+
+    if (!email || !password || !fname || !lname) {
+        response.message = 'fail'
+        return res.status(400).json({response})
+    }
+})
 
 app.listen(PORT, () => console.log(`Server start at http://localhost:${PORT}`))
